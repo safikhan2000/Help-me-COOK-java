3b6ff21381f68a8cc066cf7cfbe63ade5df6adf9
safikhan2000 <31161768+safikhan2000@users.noreply.github.com>
2019-03-10 19:07:02 +0530

    Add files via upload

---
+++ EnteringFoodList.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class EnteringFoodList {
+	private static String s1="FoodList.txt";
+	public void entering_name() throws IOException
+	{
+		BufferedWriter out = new BufferedWriter(new FileWriter(s1,true));
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter the new food item");
+		String foodItem = in.readLine();
+		System.out.println("Please enter the primary ingredient of the food item");
+		String primaryIngred = in.readLine();
+		System.out.println("Please enter the secondary ingredient of the food item");
+		String secondIngred = in.readLine();
+		String finalEntry = foodItem + "," + primaryIngred + "," + secondIngred;
+		//System.out.println(finalEntry);
+		
+		out.append(finalEntry);
+		out.newLine();
+		out.close();
+		
+		
+		
+	}
+
+
+    /*public static void main(String args[]) throws IOException
+    {
+	   EnteringFoodList obj = new EnteringFoodList();
+	   obj.entering_name();
+    }*/
+	   
+	   
+	
+}
+
---
+++ EnteringIngred.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class EnteringIngred {
+	private static String filename = "IngredList.txt";
+	public void EnteringIngredList() throws IOException
+	{
+		BufferedWriter out = new BufferedWriter(new FileWriter(filename,true));
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("please enter the ingredients present in the house");
+		String ingred = in.readLine();
+		System.out.println("Please enter the number of units of the ingredient");
+		String units = in.readLine();
+		String finalEntry = ingred + "," + units;
+		out.append(finalEntry);
+		out.newLine();
+		out.close();
+		
+		
+	}
+	
+	public static void main(String args[]) throws IOException
+	{
+		EnteringIngred obj = new EnteringIngred();
+		obj.EnteringIngredList();
+		
+	}
+
+}
